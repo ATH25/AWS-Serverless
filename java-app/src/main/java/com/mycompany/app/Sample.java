a4d0ed4a2bbc310b293256193f00a7f3bd938020
EC2 Default User <ec2-user@ip-172-31-11-144.ec2.internal>
2023-06-25 15:18:46 +0000

    check in working code after ex 5

---
+++ java-app/src/main/java/com/mycompany/app/Sample.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.app;
+
+// write a class to read every line from a file and print in the console
+
+
+public class Sample {
+
+
+     public static void public static void main (String[] args) {
+      
+        /*read a file*/ 
+        static public final String fileName = "sample_text_file";
+        
+        
+        
+     }
+    
+    
+    
+}
+
+
